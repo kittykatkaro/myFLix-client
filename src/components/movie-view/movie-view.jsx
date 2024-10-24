c9eef22e01d505484b6ceb7b736f9f6395eb02f1
Karo <karoogorek@gmail.com>
2024-10-24 12:03:10 +0200

    Add MovieView component for displaying movie details and handling back button click

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,26 @@
+export const MovieView = ({ movieData, onBackClick }) => {
+	return (
+		<div>
+			<div>
+				<img src={movieData.image} />
+			</div>
+			<div>
+				<span>Title: </span>
+				<span>{movieData.Title}</span>
+			</div>
+			<div>
+				<span>Director: </span>
+				<span>{movieData.Director}</span>
+			</div>
+			<div>
+				<span>Genre: </span>
+				<span>{movieData.Genre}</span>
+			</div>
+			<div>
+				<span>Description: </span>
+				<span>{movieData.Description}</span>
+			</div>
+			<button onClick={onBackClick}>Back</button>
+		</div>
+	);
+};
