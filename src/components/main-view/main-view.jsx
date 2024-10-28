44de7a32fefb7ef47156a130d59510cee33f527b
Karo <karoogorek@gmail.com>
2024-10-28 16:16:15 +0100

    Refactor MainView component to use useEffect hook for fetching movies from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-	const [movies, setMovies] = useState([
-		{
-			id: 1,
-			Title: 'Inception',
-			Director: 'Christopher Nolan',
-			Genre: 'Action, Adventure, Sci-Fi',
-			Description:
-				'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-			image: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg',
-		},
-		{
-			id: 2,
-			Title: 'The Shawshank Redemption',
-			Director: 'Frank Darabont',
-			Genre: 'Drama',
-			Description:
-				'A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.',
-			image: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
-		},
-		{
-			id: 3,
-			Title: 'The Godfather',
-			Director: 'Francis Ford Coppola',
-			Genre: 'Crime, Drama',
-			Description:
-				'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-			image: 'https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg',
-		},
-	]);
+	const [movies, setMovies] = useState([]);
 
 	const [selectedMovie, setSelectedMovie] = useState(null);
+
+	useEffect(() => {
+		fetch('https://my-flix-2-a94518576195.herokuapp.com/movies')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log('movies from api', data);
+				const moviesFromApi = data.map((movie) => ({
+					id: movie._id,
+					Title: movie.title,
+					Description: movie.description,
+					Genre: movie.genre.name,
+					Director: movie.director.name,
+					image: movie.imagePath,
+				}));
+				setMovies(moviesFromApi);
+			});
+	});
 
 	if (selectedMovie)
 		return (
