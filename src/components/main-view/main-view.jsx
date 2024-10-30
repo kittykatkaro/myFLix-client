fee01f680fc3dd4ff8977f42080cc9667aa50de2
Karo <karoogorek@gmail.com>
2024-10-31 00:23:12 +0100

    Implement user authentication in MainView with Login and Signup components, and manage user state with local storage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
+	const storedUser = JSON.parse(localStorage.getItem('user'));
+	const storedToken = localStorage.getItem('token');
+	const [user, setUser] = useState(storedUser ? storedUser : null);
+	const [token, setToken] = useState(storedToken ? storedToken : null);
 	const [movies, setMovies] = useState([]);
 	const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -18,11 +24,41 @@ export const MainView = () => {
 					Description: movie.description,
 					Genre: movie.genre.name,
 					Director: movie.director.name,
-					image: movie.imagePath,
+					Image: movie.image,
 				}));
 				setMovies(moviesFromApi);
 			});
 	}, []);
+
+	//authenticate requests to API through user token
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
+
+		fetch('https://my-flix-2-a94518576195.herokuapp.com/movies', {
+			headers: { Authorization: `Bearer ${token}` },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+	}, [token]);
+
+	if (!user) {
+		return (
+			<>
+				<LoginView
+					onLoggedIn={(user, token) => {
+						setUser(user);
+						setToken(token);
+					}}
+				/>
+				or
+				<SignupView />
+			</>
+		);
+	}
 
 	if (selectedMovie)
 		return (
@@ -47,6 +83,16 @@ export const MainView = () => {
 					}}
 				/>
 			))}
+
+			<button
+				onClick={() => {
+					setUser(null);
+					setToken(null);
+					localStorage.clear();
+				}}
+			>
+				Logout
+			</button>
 		</div>
 	);
 };
