a6e57e49605d69d8346c745f0933b9d3cd807610
Karo <karoogorek@gmail.com>
2024-11-01 14:37:09 +0100

    Refactor MovieCard to use React-Bootstrap components for improved layout and styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 export const MovieCard = ({ movieData, onMovieClick }) => {
 	return (
-		<div
-			onClick={() => {
-				onMovieClick(movieData);
-			}}
-		>
-			{movieData.Title}
-		</div>
+		<Card>
+			<Card.Img variant="top" src={movieData.Image} />
+			<Card.Body>
+				<Card.Title>{movieData.Title}</Card.Title>
+				<Card.Text>{movieData.Description}</Card.Text>
+				<Button onClick={() => onMovieClick(movieData)} variant="link">
+					Open
+				</Button>
+			</Card.Body>
+		</Card>
 	);
 };
 
