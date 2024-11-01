f804d61932893c5b923dc7aca9b65a59c5262f93
Karo <karoogorek@gmail.com>
2024-11-01 16:23:19 +0100

    Refactor index.jsx to remove unused Bootstrap import and simplify Container styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-// import statement to indicate to bundle '.\index.scss' file
 import './index.scss';
 
 // main component
 const MyFlixApp = () => {
 	return (
-		<Container style={{ border: '1px solid red' }}>
+		<Container>
 			<MainView />
 		</Container>
 	);
