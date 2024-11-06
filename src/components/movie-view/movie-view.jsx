1491d472b02b98e14416a4bd71f0b1632a7ed363
Karo <karoogorek@gmail.com>
2024-11-06 23:08:55 +0100

    Fix movie retrieval in MovieView component to use correct property for movie ID

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,28 +4,28 @@ import { useParams } from 'react-router-dom';
 
 export const MovieView = ({ movies }) => {
 	const { movieId } = useParams();
-	const movie = movies.find((movie) => movie._id === movieId);
+	const movie = movies.find((movie) => movie.id === movieId);
 
 	return (
 		<div>
 			<div>
-				<img src={movies.Image} />
+				<img src={movie.Image} />
 			</div>
 			<div>
 				<span>Title: </span>
-				<span>{movies.Title}</span>
+				<span>{movie.Title}</span>
 			</div>
 			<div>
 				<span>Director: </span>
-				<span>{movies.Director}</span>
+				<span>{movie.Director}</span>
 			</div>
 			<div>
 				<span>Genre: </span>
-				<span>{movies.Genre}</span>
+				<span>{movie.Genre}</span>
 			</div>
 			<div>
 				<span>Description: </span>
-				<span>{movies.Description}</span>
+				<span>{movie.Description}</span>
 			</div>
 			<Link to={'/'}>
 				<button className="back-button">Back</button>
